c1eceda1e46ba869ab7b2adb4a56bdaf04ea0ca6
Abhishek Goenka <govindkaran@gmail.com>
2019-01-13 20:03:32 +0530

    Lab1 exercise updated

--- angular/lab1/my-project/src/app/app-routing.module.ts
+++ angular/lab1/my-project/src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DataEntryComponent } from './data-entry/data-entry.component';
 import { ReportComponent } from './report/report.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'entry', component: DataEntryComponent },
+  { path: 'report', component: ReportComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/entry',
-        pathMatch: 'full'
-      },
-      { path: 'entry', component: DataEntryComponent },
-      { path: 'report', component: ReportComponent }
-    ])
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
--- angular/lab1/my-project/src/app/app.module.ts
+++ angular/lab1/my-project/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { DataEntryComponent } from './data-entry/data-entry.component';
 import { ReportComponent } from './report/report.component';
-import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     DataEntryComponent,
-    ReportComponent
+    ReportComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
